81ecc8a3165a6619306c5816e1decb434b5139de
rabindra-basnet <rabindrabasnet82@gmail.com>
2025-05-19 16:49:03 +0545

    added

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const repoName = "create-resume"; // change this to your actual GitHub repo name
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
-  // trailingSlash: true,
-  // basePath: `/${repoName}`,
-  // assetPrefix: `/${repoName}/`,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
